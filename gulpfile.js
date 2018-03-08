3d04b9187ab0527d926be78cd4a72037f0843932
liuy666 <472961306@qq.com>
2018-03-08 14:51:04 +0800

    添加gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
+// 引入需要的功能模块
 let gulp = require("gulp"),
 	htmlmin = require("gulp-htmlmin"),
 	connect = require("gulp-connect"),
-	css = require("gulp-clean-css"),
 	uglify = require("gulp-uglify"),
 	imagemin = require("gulp-imagemin"),
-	sass = require("gulp-sass");
+	sass = require("gulp-sass"),
+	babel = require("gulp-babel");
 
+// 启动服务器
 gulp.task("connect",function(){
 	connect.server({
 		root : "dist",
@@ -13,45 +15,61 @@ gulp.task("connect",function(){
 	});
 });
 
+// 将模拟假数据、js库、php文件 复制到 dist目录下
+gulp.task("mock",function(){
+	gulp.src("mock/**/*.*")
+		.pipe(gulp.dest("dist/mock"));
+});
+gulp.task("lib",function(){
+	gulp.src("lib/**/*.*")
+		.pipe(gulp.dest("dist/lib"));
+});
+gulp.task("php",function(){
+	gulp.src("php/**/*.*")
+		.pipe(gulp.dest("dist/php"));
+});
+gulp.task("copyfile",["mock","lib","php"]);
+
+// 定义scss编译压缩任务
 gulp.task("sass",function(){
-	gulp.src("sass/*.scss")
-		.pipe(sass())
-		.pipe(gulp.dest("css"))
+	gulp.src("sass/**/*.scss")
+		.pipe(sass({outputStyle : "compressed"}))
+		.pipe(gulp.dest("dist/css"))
 		.pipe(connect.reload());
 });
 
-gulp.task("indexmin",function(){
-	gulp.src("index.html")
-		.pipe(htmlmin({collapseWhitespace : true}))
-		.pipe(gulp.dest("dist"));
-});
-
+// 定义html压缩任务
 gulp.task("htmlmin",function(){
-	gulp.src("html/*.html")
-		.pipe(htmlmin({collapseWhitespace : true}))
-		.pipe(gulp.dest("dist/html"));
+	gulp.src(["**/*.html","!node_modules/**/*.html"])
+		.pipe(htmlmin({collapseWhitespace : true, minifyCSS : true, minifyJS : true}))
+		.pipe(gulp.dest("dist"))
+		.pipe(connect.reload());
 });
 
-gulp.task("css",function(){
-	gulp.src("css/*.css")
-		.pipe(css())
-		.pipe(gulp.dest("dist/css"));
-});
-
+// 定义js压缩任务
 gulp.task("js",function(){
-	gulp.src("js/*.js")
-		.pipe(uglify())
-		.pipe(gulp.dest("dist/js"));
+	gulp.src("js/**/*.js")
+		.pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(uglify())
+		.pipe(gulp.dest("dist/js"))
+		.pipe(connect.reload());
 });
 
+// 定义图片压缩任务
 gulp.task("image",function(){
-	gulp.src("images/*")
+	gulp.src("images/**/*.*")
 		.pipe(imagemin())
 		.pipe(gulp.dest("dist/images"));
 });
 
+// 定义监视任务
 gulp.task("watch",function(){
-	gulp.watch("sass/*.scss",["sass"]);
+	gulp.watch("sass/**/*.scss",["sass"]);
+	gulp.watch("js/**/*.js",["js"]);
+	// gulp.watch(["**/*.html","!node_modules/**/*.html"],["htmlmin"]);
 });
 
-gulp.task("default",["sass","js","image","htmlmin","indexmin","connect","watch"]);
+// 定义默认任务
+gulp.task("default",["sass","js","image","htmlmin","copyfile","connect","watch"]);
