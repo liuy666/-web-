11391027df70d460285c15412d8c5c80f7a6a230
liuy666 <472961306@qq.com>
2018-03-08 16:27:47 +0800

    添加requirejs 配置文件

---
+++ js/config.js
@@ -0,0 +1,8 @@
+require.config({
+	baseUrl : "/lib",
+	paths : {
+		jquery : "jQuery/jquery-3.3.1.min",
+		cookie : "jquery-cookie-master/jquery.cookie",
+		template : "artTemplate-master/template"
+	}
+});
